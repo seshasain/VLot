28eca7faa37510aad5cec0fe15fdf6d584cd0aff
seshasain <nseshasai5@gmail.com>
2021-04-27 15:41:36 +0530

    commit

--- app/src/main/java/com/example/vlot/MainActivity.java
+++ app/src/main/java/com/example/vlot/MainActivity.java
@@ -1,18 +1,84 @@
 package com.example.vlot;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import com.google.firebase.auth.FirebaseAuth;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MainActivity extends AppCompatActivity {
-
+    public EditText emailId, passwd;
+    Button btnSignUp;
+    TextView signIn;
+    FirebaseAuth firebaseAuth;
+    public void onStart() {
+        super.onStart();
+        if (this.firebaseAuth.getCurrentUser()!= null) {
+            startActivity(new Intent(MainActivity.this, Categories.class));
+            finish();
+        }
+    }
+    //s
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent I = new Intent(MainActivity.this, SignUp.class);
-        startActivity(I);
-        finish();
+        firebaseAuth = FirebaseAuth.getInstance();
+        emailId = findViewById(R.id.email);
+        passwd = findViewById(R.id.loginpaswd);
+        btnSignUp = findViewById(R.id.btnlogin);
+        signIn = findViewById(R.id.singup);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String emailID = emailId.getText().toString();
+                String paswd = passwd.getText().toString();
+                if (emailID.isEmpty()) {
+                    emailId.setError("Provide your Email first!");
+                    emailId.requestFocus();
+                } else if (paswd.isEmpty()) {
+                    passwd.setError("Set your password");
+                    passwd.requestFocus();
+                } else if (emailID.isEmpty() && paswd.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Fields Empty!", Toast.LENGTH_SHORT).show();
+                } else if (!(emailID.isEmpty() && paswd.isEmpty())) {
+                    firebaseAuth.createUserWithEmailAndPassword(emailID, paswd).addOnCompleteListener(MainActivity.this, new OnCompleteListener() {
+                        @Override
+                        public void onComplete(@NonNull Task task) {
+
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(MainActivity.this.getApplicationContext(),
+                                        "SignUp unsuccessful: " + task.getException().getMessage(),
+                                        Toast.LENGTH_SHORT).show();
+                            } else {
+                                Intent I = new Intent(MainActivity.this, Categories.class);
+                                startActivity(I);
+                                finish();
+                            }
+                        }
+                    });
+                } else {
+                    Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent I = new Intent(MainActivity.this, Login.class);
+                startActivity(I);
+                finish();
+            }
+        });
     }
 }
