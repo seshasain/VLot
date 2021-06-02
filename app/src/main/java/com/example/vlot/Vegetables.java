87b4d60105d33df88d719d641676b83aa408c58e
seshasain <nseshasai>
2021-06-02 08:47:21 +0530

    background services, notifications added

--- app/src/main/java/com/example/vlot/Vegetables.java
+++ app/src/main/java/com/example/vlot/Vegetables.java
@@ -243,7 +243,6 @@ public class Vegetables extends AppCompatActivity {
                 Map<String, Object> userMap = new HashMap<>();
                 userMap.put("Vegetables",temp);
 
-
                 customers.child("8309734591").updateChildren(userMap);
             }
         });
